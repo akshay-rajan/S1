3d63a29b04af526a2537f75735efbbc3a9fa4488
Akshay <akshay@gmail.com>
2023-10-27 15:01:11 +0530

    python

---
+++ python/listcomp.py
@@ -0,0 +1,14 @@
+l1 = ['foo', 'bar', 'baz']
+l2 = ['foo', 'ball', 'abc']
+
+if len(l1) == len(l2):
+	print("The lists are of the same length!")
+	for i in range len(l1):
+		
+else:
+	print("The lists are of different length!")
+
+
+# same value
+# any value occur in both
+
