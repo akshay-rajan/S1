c3d50086884a719e78617bc3244aad668de12e4f
akshay <akshayrajan345@gmail.com>
2023-11-03 22:16:56 +0530

    move

--- q9_str2.py
+++
@@ -1,6 +0,0 @@
-# Create a string from a given string with first and last characters replaced
-
-string = input("Enter the string: ")
-string2 = string[len(string) - 1] + string[1:len(string) - 2] + string[0]
-
-print(string2)
