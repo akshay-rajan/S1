7ef70640b665b721d14ed0ab39ca4789e959504c
akshay <akshayrajan345@gmail.com>
2023-11-03 22:03:18 +0530

    char replace

--- q8_str.py
+++ q8_str.py
@@ -4,10 +4,10 @@
 string = input("Enter the string: ")
 
 # Replace every occurence of first character with "$" in a list
-string2 = [x if x != string[0] else '$' for x in string]
+listofwords = [x if x != string[0] else '$' for x in string]
 
 # Insert the first character at the starting of the list
-string2 = [string[0]] + string2[1:]
+listofwords = [string[0]] + listofwords[1:]
 
 # Convert the list to a string
-print("".join(string2))
+print("".join(listofwords))
