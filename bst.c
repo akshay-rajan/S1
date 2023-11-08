3777bbca7513ff7e69c28bc48eef9e87fac40171
Akshay <akshay@gmail.com>
2023-11-08 11:13:04 +0530

    bst incomplete

--- bst.c
+++ bst.c
@@ -16,6 +16,7 @@ int isEmpty = 1;
 
 
 int insert();
+int delete();
 int preOrder(struct node *);
 int postOrder(struct node *);
 int inOrder(struct node *);
@@ -25,7 +26,7 @@ void main()
 {
     while (1) {
         int choice;
-        printf("Enter 0 to display, 1 to insert, 5 to exit:");
+        printf("Enter 0 to display, 1 to insert, 2 to delete, 5 to exit:");
         scanf("%d", &choice);
         switch(choice) {
             case 0:
@@ -40,6 +41,9 @@ void main()
             case 1:
                 insert();
                 break;
+			case 2:
+				delete();
+				break;
             case 5:
                 return;
             default:
@@ -52,7 +56,7 @@ void main()
 // Insert an element into the tree
 int insert()
 {
-    int value, left;
+    int value;
     struct node *newnode, *temp;
     printf("Element: ");
     scanf("%d", &value);
@@ -89,6 +93,62 @@ int insert()
         }
     }
     return 0;
+}
+
+
+// Delete an element of a particular value
+int delete()
+{
+	// Read the value to delete
+	int value, left;
+	printf("Enter the value to delete: ");
+	scanf("%d", &value);
+
+	// Find the node with the value
+	struct node *temp, *temp1;
+	temp = head;
+    while (temp != NULL) {
+        if (temp->data > value) {
+			left = 1;
+			temp1 = temp;
+            temp = temp->left;
+        }
+        else if (temp->data < value) {
+			left = 0;
+			temp1 = temp;
+            temp = temp->right;
+        }
+		else {
+			// We have reached the node to delete
+			if (left == 1) {	// If the node is the left child of its parent
+				// Replace the node with one of its child
+				if (temp->left != NULL) {
+					temp1->left = temp->left;				
+				}
+				else if (temp->right != NULL) {
+					temp1->left = temp->left;				
+				}
+				else {
+					temp1->left = NULL;
+				}
+			} else {			// If the node is the right child of its parent
+				// Replace the node with one of its child
+				if (temp->left != NULL) {
+					temp1->right = temp->left;				
+				}
+				else if (temp->right != NULL) {
+					temp1->right = temp->left;				
+				} else {
+					temp1->right = NULL;
+				}
+			}
+			free(temp);
+			return 0;
+		}
+		printf("Cannot find the node!\n");
+		return 1;
+    }
+	
 }
 
 
