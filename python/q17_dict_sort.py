0bd930f5ab57c148cec96e2b59ebbf661c026d35
Akshay <akshay@gmail.com>
2023-11-22 11:18:48 +0530

    dict-desc

--- python/q17_dict_sort.py
+++ python/q17_dict_sort.py
@@ -1,5 +1,6 @@
 dictionary = {5: "aejhro", 2: "soeiu", 6: "eiie", 1: "ojo"}
 
 
-print(dict(sorted(dictionary.items())))
+print("Ascending: ", dict(sorted(dictionary.items())))
+print("Descending: ", dict(sorted(dictionary.items(), reverse=True)))
 
