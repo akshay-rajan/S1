fc855ced5260c75d2053f5c8e449686250e33e60
akshay <akshayrajan345@gmail.com>
2023-11-27 23:25:48 +0530

    fix bug

--- python/q9_str2.py
+++ python/q9_str2.py
@@ -1,6 +1,6 @@
 # Create a string from a given string with first and last characters replaced
 
 string = input("Enter the string: ")
-string2 = string[-1] + string[1:len(string) - 2] + string[0]
+string2 = string[-1] + string[1:-1] + string[0]
 
 print(string2)
