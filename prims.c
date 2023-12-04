38a9875b84b803d61ff6bb7498e35ad104816135
akshay <akshayrajan345@gmail.com>
2023-12-04 20:48:01 +0530

    prims final

--- prims.c
+++ prims.c
@@ -1,7 +1,10 @@
+// To find the Minimum Cost Spanning Tree of a graph
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
-void main() 
+void main()
 {
     int n;
     printf("Enter the number of nodes: ");
@@ -14,41 +17,45 @@ void main()
     }
 
     // Read the Adjacency matrix
-    int A[n][n];
-    int min = 999;
-    int u, v;
+    int A[n][n], u, v;
+    int min = INT_MAX;
+    int cost = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             printf("A[%d][%d]: ", i, j);
             scanf("%d", &A[i][j]);
             // If there is no edge between two nodes, assign a big value to it to ignore it
             if (A[i][j] == 0)
-                A[i][j] = 999;
-            // Note the minimum value
+                A[i][j] = INT_MAX;
+            // Note the minimum value - edge with lowest cost to start with
             if (A[i][j] < min) {
+                min = A[i][j];
                 u = i;
                 v = j;
             }
         }
     }
     
-    int cost = 0;
     int e = 1;
     V[u] = 1;
     V[v] = 1;
-    cost += A[u][v];
+    cost += min;
     printf("{%d, %d} = %d\n", u, v, min);
 
-    // Repeat for each edge in the MST
-    while (e < n) {
+    // Repeat for each edge in the MST:
+    while (e < n - 1) {
+        min = INT_MAX;
+        // For each row in the matrix
         for (int i = 0; i < n; i++) {
+            // For each column in that row
             for (int j = 0; j < n; j++) {
-                min = 999;
-                // Check if the node i is visited
+                // Only checking for rows representing visited nodes, i.e. nodes adjascent to visited nodes
                 if (V[i] == 1) {
+                    // If the cost is less than minimum
                     if (A[i][j] < min) {
-                        // If the node j is not visited
+                        // If the node with the minimum cost is not already visited
                         if (V[j] != 1) {
+                            // Update the minimum
                             min = A[i][j];
                             u = i;
                             v = j;
@@ -57,6 +64,8 @@ void main()
                 }
             }
         }
+        // Mark the newly added minimum node as visited and print the edge of the spanning tree
+        V[v] = 1;
         printf("{%d, %d} = %d\n", u, v, min);
         cost += min;
 		e++;
