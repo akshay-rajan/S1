8c09c3b0facc52ef588aac40d3782bacdfc871f1
akshay <akshayrajan345@gmail.com>
2023-12-05 19:19:23 +0530

    kruskal incomplete

--- kruskals.c
+++ kruskals.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <limits.h>
 
+#define MAX_NODES 100
+
+int DFS(int node, int n, int graph[][MAX_NODES], int visited[], int parent);
+
 
 void main() 
 {
@@ -9,8 +13,8 @@ void main()
     printf("Enter the number of nodes: ");
     scanf("%d", &n);
 
-    // Read the Adjacency matrix, also creating a flattened version of the 2D array for sorting
-    int A[n][n], u, v;
+    // Read the Adjacency matrix, also creating a flattened version of it for sorting
+    int A[MAX_NODES][MAX_NODES], u, v;
     int n2 = n * n;
     int toSort[n2];
     int sorted[n2];
@@ -42,27 +46,12 @@ void main()
 
     // Construct the MST, avoiding cycles (DFS)
 
-    // Visited Array
-    int V[n];
-    for (int i = 0; i < n; i++) {
-        V[i] = 0;
-    }
-
-    // DFS Function
-    void DFS(int u, int A[n][n]) {
-        V[u] = 1;
-        for (int i = 0; i < n; i++) {
-            if (A[u][i] != INT_MAX && V[i] != 1) {
-                DFS(i, A);
-            }
-        }
-    };
-
     // MST
     int mst[n][n];
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            mst[i][j] = INT_MAX;
+            // Initially, set all entries to 0
+            mst[i][j] = 0;
         }
     }
     for (int i = 0; i < n2; i++) {
@@ -71,45 +60,53 @@ void main()
             if (sorted[i] == toSort[j]) {
                 int row = j / n;
                 int col = j % n;
-                mst[row][col] = sorted[j];
-                mst[col][row] = sorted[i];
-                // if (A[row][col] != INT_MAX)
-                //     printf("{%d, %d} = %d\n", row, col, A[row][col]);
+                // If the edge does not exist
+                if (A[row][col] == INT_MAX)
+                    mst[row][col] = 0;
+                else
+                    mst[row][col] = sorted[i];
+
+                // Make sure there are no cycles using DFS
+                int V[n];
+                for (int i = 0; i < n; i++) {
+                    V[i] = 0;
+                }
+                if (DFS(0, n, mst, V, -1)) {
+                    // Cycle found
+                    printf("Cycle found!\n");
+                    mst[row][col] = 0;
+                }                
                 toSort[j] = INT_MAX;
                 break;
             }
         }
-        // Removing cycles from the mst
-        for (int k = 0; k < n; k++) {
-            V[k] = 0;
-        }
-        DFS(0, mst);
-        int cycle = 0;
-        for (int k = 0; k < n; k++) {
-            if (V[k] == 0) {
-                cycle = 1;
-                break;
-            }
-        }
-        if (cycle) {
-            int row = (i + 1) / n;
-            int col = (i + 1) % n;
-            mst[row][col] = INT_MAX;
-            mst[col][row] = INT_MAX;
-        }
     }
-
     // Print the MST
     printf("Minimum Spanning Tree: \n");
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            if (mst[i][j] == INT_MAX) {
-                printf("0 ");
-                continue;
-            }
             printf("%d ", mst[i][j]);
         }
         printf("\n");
     }
 }
 
+
+// DFS Function: To detect Cycle in a graph
+int DFS(int node, int n, int graph[][MAX_NODES], int visited[], int parent) {
+    visited[node] = 1;
+
+    for (int i = 0; i < n; ++i) {
+        if (graph[node][i] != 0) {
+            if (!visited[i]) {
+                if (DFS(i, n, graph, visited, node)) {
+                    return 1; // Cycle detected
+                }
+            } else if (i != parent) {
+                // If the neighbor is already visited and not the parent, a cycle is detected
+                return 1;
+            }
+        }
+    }
+    return 0; // No cycle detected
+}
