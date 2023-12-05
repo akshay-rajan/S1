b31712c150409fb5f23b95629c87a8c0a1be4f68
akshay <akshayrajan345@gmail.com>
2023-12-05 12:20:16 +0530

    dfs final

--- dfs.c
+++ dfs.c
@@ -24,7 +24,7 @@ void main()
 
     // Perform DFS starting from a node 'u' 
     // (A Stack works in the background of the recursive algorithm)
-    void dfs(int u) {
+    void DFS(int u) {
         printf("%d ", u);
         V[u] = 1;
         // For each node adjacent to u
@@ -32,12 +32,12 @@ void main()
             if (A[u][i] != 0) {
                 // If the node is not already visited, perform DFS on it
                 if (V[i] == 0)
-                    dfs(i);
+                    DFS(i);
             }
         }
     }
 
     printf("Performing DFS starting from the first vertex: \n");
-    dfs(0);
+    DFS(0);
     printf("\n");
 }
