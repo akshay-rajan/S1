70f99bac3b7d33ace5c107895c8034cf0b31ddaa
Akshay <akshay@gmail.com>
2023-12-07 13:44:03 +0530

    py 4 qn complete

--- python/class/bank.py
+++ python/class/bank.py
@@ -8,10 +8,14 @@ class Account:
 	
 	def deposit(self, amount):
 		self.bal += amount
+		print(f"Deposited Rs.{amount} successfully!")
 		
 	def withdraw(self, amount):
-		if amount <= self.bal:
-			self.bal -= amount
+		if amount > self.bal:
+			print("Insufficient balance!")
+			return		
+		self.bal -= amount
+		print(f"Withdrawed Rs.{amount} successfully!")
 
 account_no = int(input("Enter the account no: "))
 name = input("Enter the name: ")
@@ -20,7 +24,7 @@ account_type = input("Enter the account type: ")
 member = Account(account_no, name, account_type, 0)
 
 while True:
-	print("Name:")
+	print(f"Name: {member.name}\nAccount No.: {member.acc_no}\nAccount type: {member.ac_type}\nBalance: {member.bal}")
 	transaction = int(input("Enter 1 to deposit, 2 to withdraw: "))
 	if transaction == 1:
 		member.deposit(int(input("Enter the amount to deposit: ")))
