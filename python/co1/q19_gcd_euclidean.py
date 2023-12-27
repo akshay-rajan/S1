2c3e00192071ac474e502f4932280a0fd1ae8da8
Akshay Rajan <akshayrajan345@gmail.com>
2023-12-27 20:39:43 +0530

    Optimisation

--- python/co1/q19_gcd_euclidean.py
+++ python/co1/q19_gcd_euclidean.py
@@ -3,9 +3,7 @@ b = int(input("Number 2: "))
     
 # Repeatedly replacing the larger number by its remainder when divided by the smaller number
 while b > 0:
-    r = a % b
-    a = b
-    b = r
+    a, b = b, a % b
     
 print(f"GCD :", a)
     
