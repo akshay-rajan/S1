538f5a3185276716b2a270e744d9e4db16f71993
Akshay Rajan <akshayrajan345@gmail.com>
2023-12-27 20:42:09 +0530

    comment

--- python/co1/q19_gcd_euclidean.py
+++ python/co1/q19_gcd_euclidean.py
@@ -1,9 +1,10 @@
 a = int(input("Number 1: "))
 b = int(input("Number 2: "))
     
-# Repeatedly replacing the larger number by its remainder when divided by the smaller number
+# Euclidean Algorithm: Repeatedly replacing the second number by its remainder when divided by the first number
 while b > 0:
     a, b = b, a % b
-    
+
+# GCD is the last non zero remainder
 print(f"GCD :", a)
     
