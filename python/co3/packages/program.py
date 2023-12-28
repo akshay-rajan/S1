95addb876b12a83ac921ca0198ab72892b75bf8b
Akshay Rajan <akshayrajan345@gmail.com>
2023-12-28 21:26:18 +0530

    Modules

--- python/co3/packages/program.py
+++ python/co3/packages/program.py
@@ -4,12 +4,16 @@ from graphics.graphics3d import cuboid
 from graphics.graphics3d.sphere import area as sphere_area
 from graphics.graphics3d.sphere import perimeter as sphere_perimeter
 
-print("Area of the circle: ", circle.area(int(input("Enter the radius of the circle: "))))
-print("Perimeter of the circle: ", circle.perimeter(int(input("Enter the radius of the circle: "))))
-print("Area of the rectangle: ", area(int(input("Length: ")), int(input("Breadth: "))))
-print("Perimeter of the rectangle: ", perimeter(int(input("Length: ")), int(input("Breadth: "))))
-print("Area of the sphere: ", sphere_area(int(input("Enter the radius of the sphere: "))))
-print("Perimeter of the sphere: ", sphere_perimeter(int(input("Enter the radius of the sphere: "))))
-print("Area of the cuboid: ", cuboid.area(int(input("Length: ")), int(input("Breadth: ")), int(input("Height: "))))
-print("Perimeter of the rectangle: ", cuboid.perimeter(int(input("Length: ")), int(input("Breadth: ")), int(input("Height: "))))
+print("Circle with Radius 5")
+print("Area: ", circle.area(5))
+print("Perimeter: ", circle.perimeter(5))
+print("Rectangle with Length: 6, Breadth: 4")
+print("Area: ", area(6, 4))
+print("Perimeter: ", perimeter(6, 4))
+print("Sphere with Radius 4")
+print("Area: ", sphere_area(4))
+print("Perimeter: ", sphere_perimeter(4))
+print("Cuboid with Length: 6, Breadth: 4, Height: 3")
+print("Area: ", cuboid.area(6, 4, 3))
+print("Perimeter: ", cuboid.perimeter(6, 4, 3))
 
