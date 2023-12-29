bc8120b2fe9ad648e1f084ab3e1a40bfbb627bc3
Akshay Rajan <akshayrajan345@gmail.com>
2023-12-29 22:24:17 +0530

    Update set reduce loc

--- ads/set.c
+++ ads/set.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
 
-
 int isPresent(int arr[], int size, int value);
+void readSet(int set[], int universal[], int size);
+void printSet(int set[], int size);
+
+
+int n, n1, n2;
 
 
 void main() 
 {
-	int n, n1, n2, element;
-
-	// Read the elements in the universal set
+	// Read the Universal set
 	printf("Enter the size of the universal set: ");
 	scanf("%d", &n);
 	int U[n];
-	for (int i = 0; i < n; i++) {
-		printf("Element %d: ", i + 1);
-		scanf("%d", &U[i]);
-		if (isPresent(U, i, U[i])) {
-			printf("Element already exists!\n");
-			i--;
-			continue;
-		}
-	}
+	printf("Enter the elements in the Universal Set: \n");
+	readSet(U, NULL, n);
 
 	// Read the elements in the two sets
 	printf("Enter the size of set 1: ");
@@ -31,17 +26,7 @@ void main()
 	}
 	int set1[n1];
 	printf("Enter the elements in set 1: \n");
-	for (int i = 0; i < n1; i++) {
-		printf("set1[%d]: ", i);
-		scanf("%d", &element);
-		// If the element is unique in the current set, and present in the universal set
-		if (!isPresent(set1, i, element) && isPresent(U, n, element))
-			set1[i] = element;
-		else {
-			printf("Element must be unique and present in the universal set!\n");
-			i--;
-		}	
-	}
+	readSet(set1, U, n1);
 	printf("Enter the size of set 2: ");
 	scanf("%d", &n2);
 	if (n2 > n || n2 < 0) {
@@ -50,16 +35,7 @@ void main()
 	}
 	int set2[n2];
 	printf("Enter the elements in set 2: \n");
-	for (int i = 0; i < n2; i++) {
-		printf("set2[%d]: ", i);
-		scanf("%d", &element);
-		if (!isPresent(set2, i, element) && isPresent(U, n, element))
-			set2[i] = element;
-		else {
-			printf("Element must be unique and present in the universal set!\n");
-			i--;
-		}	
-	}
+	readSet(set2, U, n2);
 
 	// Represent the sets using bit vectors
 	int S1[n], S2[n];
@@ -99,21 +75,15 @@ void main()
 
 	// Print the sets
 	printf("U    : ");
-	for (int i = 0; i < n; i++)
-		printf("%d ", U[i]);
-	printf("\nSet 1: ");
-	for (int i = 0; i < n; i++)
-		printf("%d ", S1[i]);
-	printf("\nSet 2: ");
-	for (int i = 0; i < n; i++)
-		printf("%d ", S2[i]);
-	printf("\nS1uS2: ");
-	for (int i = 0; i < n; i++)
-		printf("%d ", setUnion[i]);
-	printf("\nS1nS2: ");
-	for (int i = 0; i < n; i++)
-		printf("%d ", setIntersection[i]);
-	printf("\n");
+	printSet(U, n);
+	printf("Set 1: ");
+	printSet(S1, n);
+	printf("Set 2: ");
+	printSet(S2, n);
+	printf("S1uS2: ");
+	printSet(setUnion, n);
+	printf("S1nS2: ");
+	printSet(setIntersection, n);
 }
 
 
@@ -124,6 +94,30 @@ int isPresent(int arr[], int size, int value) {
 			return 1;
 	}
 	return 0;
+}
+
+// Read a set
+void readSet(int set[], int universal[], int size) {
+	int element;
+	for (int i = 0; i < size; i++) {
+		printf("Element %d: ", i + 1);
+		scanf("%d", &element);
+		// If the element is not already in the set and is present in the universal set or if we are reading the Universal set itself
+		if (!isPresent(set, i, element) && (universal == NULL || isPresent(universal, n, element))) {
+			set[i] = element;
+		} else {
+			printf("Invalid Entry!\n");
+			i--;
+		}
+	}
+}
+
+// Print a set
+void printSet(int set[], int size) {	
+	for (int i = 0; i < size; i++) {
+		printf("%d ", set[i]);
+	}
+	printf("\n");
 }
 
 
