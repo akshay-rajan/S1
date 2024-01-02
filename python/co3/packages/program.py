e2b1edfa682cef543a56da6491035c1272142c50
Akshay Rajan <akshayrajan345@gmail.com>
2024-01-02 21:26:08 +0530

    Use Multi-Line Strings

--- python/co3/packages/program.py
+++ python/co3/packages/program.py
@@ -4,16 +4,16 @@ from graphics.graphics3d import cuboid
 from graphics.graphics3d.sphere import area as sphere_area
 from graphics.graphics3d.sphere import perimeter as sphere_perimeter
 
-print("Circle with Radius 5")
-print("Area: ", circle.area(5))
-print("Perimeter: ", circle.perimeter(5))
-print("Rectangle with Length: 6, Breadth: 4")
-print("Area: ", area(6, 4))
-print("Perimeter: ", perimeter(6, 4))
-print("Sphere with Radius 4")
-print("Area: ", sphere_area(4))
-print("Perimeter: ", sphere_perimeter(4))
-print("Cuboid with Length: 6, Breadth: 4, Height: 3")
-print("Area: ", cuboid.area(6, 4, 3))
-print("Perimeter: ", cuboid.perimeter(6, 4, 3))
-
+print(f"""Circle with Radius 5
+Area: , {circle.area(5)}
+Perimeter: {circle.perimeter(5)}
+Rectangle with Length: 6, Breadth: 4
+Area: {area(6, 4)}
+Perimeter: {perimeter(6, 4)}
+Sphere with Radius 4
+Area: {sphere_area(4)}
+Perimeter: {sphere_perimeter(4)}
+Cuboid with Length: 6, Breadth: 4, Height: 3
+Area: {cuboid.area(6, 4, 3)}
+Perimeter: {cuboid.perimeter(6, 4, 3)}"""
+)
